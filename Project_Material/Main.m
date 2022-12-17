2206db29035c4783098abe920edf6844d1c7e5a0
lorecol <lorenzo.colturaato@gmail.com>
2022-12-17 18:24:30 +0100

    updated readme

--- Project_Material/Main.m
+++ Project_Material/Main.m
@@ -10,29 +10,16 @@ cloud1 =  pcread( fullfile(mainFolder,'007086770647cloud0.ply'));
 cloud2 =  pcread( fullfile(mainFolder,'018408745047cloud0.ply'));
 
 % fullfile create a path of the specified file in the specified folder
-% pcread reads the point cloud from the specified .ply file returning a
-% pointCloud object
 
-% pointCloud object:
-%   - Location: position of the points in 3D coordinate space
-%   - Color: set the color of points in point cloud
-%   - Normal: specify the normal vector with respect to each point in the point cloud
-%   - Intensity: grayscale intensities at each point
-%   - Count: total number of points in the point cloud
-%   - XLimits, YLimits, ZLimits: range of coordinates along the 3 axes
-
-H0 = load('H0.txt'); % roto-translation matrix of frontal camera
-H1 = load('H1.txt'); % roto-translation matrix of left lateral camera
-H2 = load('H2.txt'); % roto-translation matrix of right lateral camera
+H0 = load('H0.txt'); 
+H1 = load('H1.txt'); 
+H2 = load('H2.txt'); 
 
 Hnorm = [ 0 1 0 0 ; 0 0 1 0 ; 1 0 0 0 ; 0 0 0 1 ]
 
-points0 = [cloud0.Location'; ones(1,length(cloud0.Location))]; % create a 4x4 matrix where the first 3 rows
-%                                                                are the (x,y,z) coordinates of the points 
-%                                                                in the point cloud 0, while the last row 
-%                                                                is full of ones
-points1 = [cloud1.Location'; ones(1,length(cloud1.Location))]; % same as above but for point cloud 1
-points2 = [cloud2.Location'; ones(1,length(cloud2.Location))]; % same as above but for point cloud 2
+points0 = [cloud0.Location'; ones(1,length(cloud0.Location))];
+points1 = [cloud1.Location'; ones(1,length(cloud1.Location))]; 
+points2 = [cloud2.Location'; ones(1,length(cloud2.Location))]; 
 
 points0rt = points0;
 points1rt = inv(H0) * H1 * points1;
@@ -48,13 +35,14 @@ cameratoolbar % displays a camera toolbar in the current figure that enables int
 %               of the axes camera and light
 
 figure(2), clf, hold on, grid on, axis equal
+% Recall the created function
 draw3dReferenceSystems()
 plot3( points0rt(1,:), points0rt(2,:), points0rt(3,:), '.r','markersize', 0.1)
 plot3( points1rt(1,:), points1rt(2,:), points1rt(3,:), '.g','markersize', 0.1)
 plot3( points2rt(1,:), points2rt(2,:), points2rt(3,:), '.b','markersize', 0.1)
 title("Transformated points from the cloud points")
 
-%% EQUALIZZA IMMAGINI
+%% TO EQUALIZE IMAGES
 
 fileinfo = dir('Calibration20220922_14_25_02_597/*.png');  % get list of files in current directory
 addpath("Calibration20220922_14_25_02_597")
@@ -62,7 +50,7 @@ thisfilename = fileinfo(1).name;  % current file name
 figure(3)
 I = imread(thisfilename); % reads the image from the specified file, inferring the format of the file 
 %                           from its contents
-I_eq = histeq(I*20/2.303);  % LE IMMAGINI VANNO EQUALIZZATE IN MANIERA LOGARITMICA COSI'
+I_eq = histeq(I*20/2.303); 
 
 % histeq performs histogram equalization --> for example to enhance the constrast of an intensity image; 
 % it transforms the grayscale image so that the histogram of the output grayscale image (I_eq) has 64 
--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 ## <span style="color: orange;">Explanation of Matlab files given by Alessandro Luchetti</span> 
 
 ---
+---
 
-### Main file:
+### 1. Main file:
 ```Matlab
 cloud0 =  pcread(fullfile(mainFolder, '004373465147cloud0.ply'));
 cloud1 =  pcread(fullfile(mainFolder, '007086770647cloud0.ply'));
@@ -42,9 +43,68 @@ points2rt = inv(H0) * H2 * points2;
 ```
 This is done to transform the cloud points by means of roto-translation transformations
 
-![Original](https://github.com/lorecol/SmartGate/tree/develop/Project_Material/Original_Cloud_Points.jpg)
+To see the ![Original Cloud points](https://github.com/lorecol/SmartGate/tree/develop/Project_Material/Original_Cloud_Points.jpg?raw=true)
+
+To see the ![Transformed Cloud points](https://github.com/lorecol/SmartGate/tree/develop/Project_Material/Transformed_Cloud_Points.jpg?raw=true)
 
 ---
+---
 
+### 2. Function ``` draw3dReferenceSystems( transformationMatrixToRS , name , scale, width ) ```:
 
-function
+This function is used in order to add in a 3D plot a cartesian reference system defined by the three axes x, y and z
+
+```Matlab
+try 
+    transformationMatrixToRS;
+catch 
+    transformationMatrixToRS = eye(4); 
+end
+```
+This defines **transformationMatrixToRS** as a 4x4 identity matrix
+
+```Matlab
+try
+    name;
+catch
+    name = 'reference frame';
+end
+```
+This defines the **name** of the reference system as **reference frame**
+
+```Matlab
+try
+    scale;
+catch
+    scale = 1 ;
+end
+```
+This sets the **scale** of the reference system to **1**
+
+```Matlab
+try
+    width;
+catch
+    width = 3;
+end
+```
+This sets the **width** of the arrow that define the three axes of the ref. system to **3**
+
+```Matlab
+originPoint = transformationMatrixToRS * [ 0 0 0 1 ]';
+
+xDirVector  = ( transformationMatrixToRS * [ 1 0 0 1 ]' - originPoint).* scale;
+yDirVector  = ( transformationMatrixToRS * [ 0 1 0 1 ]'  - originPoint).* scale;
+zDirVector  = ( transformationMatrixToRS * [ 0 0 1 1 ]' - originPoint).* scale;
+```
+This specifies the directions in which the three cartesian axes x, y, z are defined:
+   - x &rarr; {1, 0, 0}
+   - y &rarr; {0, 1, 0}
+   - z &rarr; {0, 0, 1}
+
+```Matlab
+px = quiver3( originPoint(1) , originPoint(2) , originPoint(3) , xDirVector(1) , xDirVector(2) , xDirVector(3) , 'r' , 'LineWidth', width);
+py = quiver3( originPoint(1) , originPoint(2) , originPoint(3) , yDirVector(1) , yDirVector(2) , yDirVector(3) , 'g' , 'LineWidth', width);
+pz = quiver3( originPoint(1) , originPoint(2) , originPoint(3) , zDirVector(1) , zDirVector(2) , zDirVector(3) , 'b' , 'LineWidth', width);
+```
+In general **_quiver3(X, Y, Z, U, V, W)_** plots arrows with directional components U, V, and W at the Cartesian coordinates specified by X, Y, and Z
