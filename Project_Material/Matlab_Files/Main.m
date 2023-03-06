fc0e7c82190d10713afa21b40a60c46aaf417486
lorecol <lorenzo.colturaato@gmail.com>
2023-03-06 15:16:50 +0100

    updated main

--- Project_Material/Matlab_Files/Main.m
+++ Project_Material/Matlab_Files/Main.m
@@ -2,7 +2,27 @@ clc
 clear all
 close all
 
-%%
+%% TO EQUALIZE IMAGES
+
+fileinfo = dir('Calibration20220922_14_25_02_597/*.png');  % get list of files in current directory
+addpath("Calibration20220922_14_25_02_597")
+thisfilename = struct('Image1', fileinfo(1).name, 'Image2', fileinfo(2).name, 'Image3', fileinfo(3).name);
+images = fieldnames(thisfilename);
+
+for i = 1:numel(images)
+    figure(i)
+    imshow(histeq((imread(fileinfo(i).name))*20/2.303))
+end
+
+% imread( ) reads the image from the specified file, inferring the format of the file from its contents
+
+% histeq( ) performs histogram equalization --> for example to enhance the constrast of an intensity image; 
+% it transforms the grayscale image so that the histogram of the output grayscale image (I_eq) has 64 
+% bins and is approximately flat
+
+% imshow( ) displays the grayscale image in a figure
+
+%% MAIN
 
 mainFolder = 'Calibration20220922_14_23_47_135';
 cloud0 =  pcread( fullfile(mainFolder,'004373465147cloud0.ply'));
@@ -26,7 +46,7 @@ points1rt = inv(H0) * H1 * points1;
 points2rt = inv(H0) * H2 * points2;
 
 % Plot points in 3D space from the three cloud points
-figure(1), clf, hold on, grid on, axis equal
+figure(4), clf, hold on, grid on, axis equal
 plot3( cloud0.Location(:,1), cloud0.Location(:,2), cloud0.Location(:,3), '.r','markersize', 0.1)
 plot3( cloud1.Location(:,1), cloud1.Location(:,2), cloud1.Location(:,3), '.g','markersize', 0.1)
 plot3( cloud2.Location(:,1), cloud2.Location(:,2), cloud2.Location(:,3), '.b','markersize', 0.1)
@@ -34,26 +54,10 @@ title("Original points from the cloud points")
 cameratoolbar % displays a camera toolbar in the current figure that enables interactive manipulation 
 %               of the axes camera and light
 
-figure(2), clf, hold on, grid on, axis equal
+figure(5), clf, hold on, grid on, axis equal
 % Recall the created function
 draw3dReferenceSystems()
 plot3( points0rt(1,:), points0rt(2,:), points0rt(3,:), '.r','markersize', 0.1)
 plot3( points1rt(1,:), points1rt(2,:), points1rt(3,:), '.g','markersize', 0.1)
 plot3( points2rt(1,:), points2rt(2,:), points2rt(3,:), '.b','markersize', 0.1)
 title("Transformated points from the cloud points")
-
-%% TO EQUALIZE IMAGES
-
-fileinfo = dir('Calibration20220922_14_25_02_597/*.png');  % get list of files in current directory
-addpath("Calibration20220922_14_25_02_597")
-thisfilename = fileinfo(1).name;  % current file name
-figure(3)
-I = imread(thisfilename); % reads the image from the specified file, inferring the format of the file 
-%                           from its contents
-I_eq = histeq(I*20/2.303); 
-
-% histeq performs histogram equalization --> for example to enhance the constrast of an intensity image; 
-% it transforms the grayscale image so that the histogram of the output grayscale image (I_eq) has 64 
-% bins and is approximately flat
-
-imshow(I_eq); % displays the grayscale image in a figure
