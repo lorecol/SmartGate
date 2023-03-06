cc0eecdadb9edf8f18398c3e14d2d1cbe205122c
lorecol <lorenzo.colturaato@gmail.com>
2023-03-06 17:46:43 +0100

    updated files

--- Project_Material/Matlab_Files/Main.m
+++ Project_Material/Matlab_Files/Main.m
@@ -2,36 +2,35 @@ clc
 clear all
 close all
 
-%% EQUALIZE IMAGES
-
-fileinfo = dir('Calibration20220922_14_25_02_597/*.png');  % get list of files in current directory
-addpath("Calibration20220922_14_25_02_597")
-thisfilename = struct('Image1', fileinfo(1).name, 'Image2', fileinfo(2).name, 'Image3', fileinfo(3).name);
-images = fieldnames(thisfilename);
-cam = [0 1 2];
-
-for i = 1:numel(images)
-    figure(i)
-    imshow(histeq((imread(fileinfo(i).name))*20/2.303))
-    title (sprintf('Cam %d', cam(i)))
-end
-
-% imread( ) reads the image from the specified file, inferring the format of the file from its contents
-
-% histeq( ) performs histogram equalization --> for example to enhance the constrast of an intensity image; 
-% it transforms the grayscale image so that the histogram of the output grayscale image (I_eq) has 64 
-% bins and is approximately flat
-
-% imshow( ) displays the grayscale image in a figure
-
 %% MAIN
 
-mainFolder = {'Calibration20220922_14_23_47_135', 'Calibration20220922_14_25_02_597'};
+% There is a script that allows the user to choose which of the 2 provided 
+% dataset to evaluate.
+% After the choice, first the images provided by the 3 cameras are
+% equalized, and then the point clouds are evaluated and displayed in a
+% graphical way
+
+mainFolder = {'Calibration20220922_14_23_47_135', 'Calibration20220922_14_25_02_597'}; % Datasets to choose from
+addpath(genpath(pwd))
 
 data_choice = input("Which data set you want to evaluate?\n - 1 for the 1st dataset\n - 2 for the 2nd dataset\n\n Choice: ");
 
 if data_choice == 1
+    
+    % Equalize camera images
+    fileinfo = dir('Project_Material/Datasets/Calibration20220922_14_23_47_135/*.png');  % get list of files in current directory
+    addpath("Project_Material/Datasets/Calibration20220922_14_23_47_135")
+    thisfilename = struct('Image1', fileinfo(1).name, 'Image2', fileinfo(2).name, 'Image3', fileinfo(3).name);
+    images = fieldnames(thisfilename);
+    cam = [0 1 2];
+    
+    for i = 1:numel(images)
+        figure(i)
+        imshow(histeq((imread(fileinfo(i).name))*20/2.303))
+        title (sprintf('Cam %d', cam(i)))
+    end
 
+    % Evaluate cloud points
     cloud0 =  pcread( fullfile(mainFolder{1},'004373465147cloud0.ply'));
     cloud1 =  pcread( fullfile(mainFolder{1},'007086770647cloud0.ply'));
     cloud2 =  pcread( fullfile(mainFolder{1},'018408745047cloud0.ply'));
@@ -71,6 +70,20 @@ if data_choice == 1
 
 elseif data_choice == 2
 
+    % Equalize camera images
+    fileinfo = dir('Project_Material/Datasets/Calibration20220922_14_25_02_597/*.png');  % get list of files in current directory
+    addpath("Project_Material/Datasets/Calibration20220922_14_25_02_597")
+    thisfilename = struct('Image1', fileinfo(1).name, 'Image2', fileinfo(2).name, 'Image3', fileinfo(3).name);
+    images = fieldnames(thisfilename);
+    cam = [0 1 2];
+    
+    for i = 1:numel(images)
+        figure(i)
+        imshow(histeq((imread(fileinfo(i).name))*20/2.303))
+        title (sprintf('Cam %d', cam(i)))
+    end
+
+    % Evaluate cloud points
     cloud0 =  pcread( fullfile(mainFolder{2},'004373465147cloud0.ply'));
     cloud1 =  pcread( fullfile(mainFolder{2},'007086770647cloud0.ply'));
     cloud2 =  pcread( fullfile(mainFolder{2},'018408745047cloud0.ply'));
@@ -109,3 +122,11 @@ elseif data_choice == 2
     title("Transformated points from the cloud points")
 
 end
+
+% imread( ) reads the image from the specified file, inferring the format of the file from its contents
+
+% histeq( ) performs histogram equalization --> for example to enhance the constrast of an intensity image; 
+% it transforms the grayscale image so that the histogram of the output grayscale image (I_eq) has 64 
+% bins and is approximately flat
+
+% imshow( ) displays the grayscale image in a figure
